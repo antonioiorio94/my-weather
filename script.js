e1e2d91a15d63dc1831c40c8cbff35a8a6b71591
Antonio Iorio <antonioiorio94@gmail.com>
2022-07-14 12:14:52 +0200

    add fetching data from OpenWeather API

---
+++ script.js
@@ -0,0 +1,20 @@
+let weather = {
+  apiKey: "6a5c71d3dedcc099361c5fb435375a77",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+  },
+};
